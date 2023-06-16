1512f30373316d18e82bb90a502d39cff90a6cc1
HugoRCD <hugo.richard@epitech.eu>
2023-06-16 22:31:19 +0200

    Add new favicon

--- app.vue
+++ app.vue
@@ -11,6 +11,27 @@ useHead({
       type: "image/x-icon",
       href: "/favicon.ico",
     },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+    {
+      rel: "manifest",
+      href: "/site.webmanifest",
+    },
   ],
 });
 </script>
