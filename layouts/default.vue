77ca049274702fb6d1c6a94ced2560439f1b85ba
HugoRCD <hugo.richard@epitech.eu>
2023-06-20 22:14:08 +0200

    Add aurora and improve language selector

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="h-full flex flex-col relative">
+  <div class="flex flex-col relative">
     <div class="absolute top-0 left-0 right-0 z-50 w-50">
-      <div class="flex items-center p-6">
-        <LanguageSelector is-text />
+      <div class="fixed flex items-center p-6">
+        <div class="bg-black/20 rounded-full backdrop-blur-3xl border-[1px] border-white/20">
+          <LanguageSelector is-text />
+        </div>
       </div>
     </div>
     <slot />
     <LayoutFooter />
   </div>
 </template>
+<script setup lang="ts"></script>
--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,8 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="flex flex-col h-full">
-    <div class="h-1/3 flex justify-center items-center p-6">
+  <div class="h-screen flex flex-col bg-main">
+    <div class="flex justify-center items-center p-6 relative h-[500px]">
       <div class="flex flex-col items-center gap-2">
         <Logo :size="20" />
         <div class="flex flex-col items-center">
@@ -18,19 +18,22 @@ definePageMeta({
           <h3 class="text-xl font-bold text-gradient">Creative Developer</h3>
         </div>
       </div>
+      <img src="/assets/aurora.png" alt="aurora" class="object-cover blur-3xl w-full h-full absolute top-0 left-0" />
     </div>
     <div class="linebreak"></div>
-    <div class="spotlight flex flex-col items-center justify-center">
-      <h1 class="main-heading mt-10 text-center">
-        <span class="white-gradient-tb">Turning ideas into real life products </span>
-        <br />
-        <span class="white-gradient-tb">is my calling</span>
-      </h1>
-      <p class="subheading mx-10 sm:mx-20 text-center">
-        I craft premium websites for creators, entrepreneurs and startups who want to convert clients, drive serious revenue and stand out in an ever-crowding
-        market.
-      </p>
-      <NuxtLink to="/contact" class="btn-primary mt-4"> Contact </NuxtLink>
+    <div class="spotlight flex flex-col items-center justify-center z-10 bg-main">
+      <div class="max-w-6xl flex flex-col items-center gap-4">
+        <h1 class="main-heading mt-16 text-center">
+          <span class="white-gradient-tb">Turning ideas into real life products </span>
+          <br />
+          <span class="white-gradient-tb">is my calling</span>
+        </h1>
+        <p class="subheading mx-10 sm:mx-20 text-center">
+          I craft premium websites for creators, entrepreneurs and startups who want to convert clients, drive serious revenue and stand out in an ever-crowding
+          market.
+        </p>
+        <NuxtLink to="/contact" class="btn-primary mt-4"> Contact </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
