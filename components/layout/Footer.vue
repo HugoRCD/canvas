93820f4683940b737574d09d4e41de0aa4c1fd2c
HugoRCD <hugo.richard@epitech.eu>
2023-08-22 22:26:42 +0200

    Update footer link

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -7,7 +7,7 @@
         >© 2023,
         <NuxtLink
           target="_blank"
-          to="https://www.maisonhochard.com"
+          to="https://www.mh.hrcd.fr"
           class="text-muted hover:text-primary transition-colors duration-300 ease-in-out"
           aria-label="Go to Maison Hochard website"
         >
