47ede9e80a351d5fb71b55561c18375992a96a95
HugoRCD <hugo.richard@epitech.eu>
2023-08-24 18:10:55 +0200

    Init contact page

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -10,17 +10,21 @@ const navigation = getNavigation("home");
 </script>
 
 <template>
-  <header class="z-10 h-[45px] bg-secondary border-[1px] border-primary shadow-md group flex justify-around px-6 py-2 gap-4 w-full">
-    <NuxtLink
-      v-for="item in navigation"
-      :key="item.name"
-      :to="item.to"
-      :id="item.name.toLowerCase()"
-      class="hover:text-primary transition-all duration-300 ease-in-out flex items-center gap-2"
-      :class="[item.name === $route.name ? 'text-primary' : 'text-muted']"
+  <div class="flex items-center justify-center mx-auto w-full sm:mt-2">
+    <header
+      class="z-10 h-[50px] sm:h-[45px] bg-secondary border-[1px] border-primary shadow-md group flex justify-around px-6 py-3 sm:py-2 gap-4 sm:gap-8 sm:rounded-full w-full sm:w-auto"
     >
-      <component :is="item.icon" class="w-6 h-6 font-medium" />
-      <span class="hidden sm:flex text-[14px]"> {{ $t("navigation." + item.name.toLowerCase()) }}</span>
-    </NuxtLink>
-  </header>
+      <NuxtLink
+        v-for="item in navigation"
+        :key="item.name"
+        :to="item.to"
+        :id="item.name.toLowerCase()"
+        class="hover:text-primary transition-all duration-300 ease-in-out flex items-center gap-2"
+        :class="[item.name === $route.name ? 'text-primary' : 'text-muted']"
+        :aria-label="item.name + ' navigation link'"
+      >
+        <component :is="item.icon" class="w-7 h-7 sm:w-6 sm:h-6 font-medium" />
+      </NuxtLink>
+    </header>
+  </div>
 </template>
