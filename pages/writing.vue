6c4f157d5a05f44351279de3bf01cc551b788db1
HugoRCD <hugo.richard@epitech.eu>
2023-08-30 20:43:26 +0200

    Improve content navigation without await

--- pages/writing.vue
+++ pages/writing.vue
@@ -1,27 +1,34 @@
 <script setup lang="ts">
-import { Article } from "~/types/Article";
-
 definePageMeta({
   name: "Writing",
   title: "Writing",
-});
-
-const articles = ref<Article[]>([]);
-
-await useAsyncData("articles", () => queryContent("/articles").find()).then((data) => {
-  articles.value = data.data.value as unknown as Article[];
 });
 </script>
 
 <template>
   <LayoutInfoWrapper page="writing">
-    <NuxtLink
+    <nav>
+      <ContentNavigation v-slot="{ navigation }">
+        <ul>
+          <li v-for="link of navigation.filter((link) => link._path === '/articles')[0].children" :key="link._path">
+            <NuxtLink
+              :to="link._path"
+              class="flex items-center gap-2 cursor-pointer px-4 py-2 rounded-lg hover:bg-secondary hover:text-primary"
+              :aria-label="link.title"
+            >
+              {{ link.title }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </ContentNavigation>
+    </nav>
+    <!--    <NuxtLink
       v-for="article of articles"
       :key="article._path"
       :to="article._path"
       class="flex items-center gap-2 cursor-pointer px-4 py-2 rounded-lg hover:bg-secondary hover:text-primary"
     >
       {{ article.title }}
-    </NuxtLink>
+    </NuxtLink>-->
   </LayoutInfoWrapper>
 </template>
