93ba38582918e24e7514dbcc0da0765c594d0b8e
HugoRCD <hugo.richard@epitech.eu>
2023-09-21 11:12:57 +0200

    Revert 🤷‍

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -85,7 +85,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: true,
-      routes: ["/sitemap.xml", "/robots.txt"],
+      routes: ["/sitemap.xml"],
     },
   },
 
