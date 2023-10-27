d5e6cd17e80bd23ea809f627440233bbeaa4a8d3
HugoRCD <hugo.richard@epitech.eu>
2023-10-27 17:24:50 +0200

    Seo update

--- components/home/Projects.vue
+++ components/home/Projects.vue
@@ -4,7 +4,7 @@ import projects from "~/data/projects";
 
 <template>
   <div class="flex flex-col gap-4 w-full">
-    <h3 class="text-muted">
+    <h3 class="text-xl font-bold font-testimonial text-white-shadow">
       {{ $t("navigation.works") }}
     </h3>
     <div class="flex flex-col gap-4 w-full">
@@ -13,7 +13,7 @@ import projects from "~/data/projects";
         :key="project.name"
         class="flex items-center gap-2 cursor-pointer px-4 py-2 rounded-lg hover:bg-secondary hover:text-main"
         :to="project.release === 'soon' ? '/' : project.link"
-        :aria-label="project.name + ' project link'"
+        :aria-label="'go to ' + project.name + ' project website'"
         :target="project.release === 'soon' ? '_self' : '_blank'"
       >
         <span class="whitespace-nowrap">
