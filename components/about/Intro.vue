0efc2e0203d771c0429a60b7491416f00fd4b171
HugoRCD <hugo.richard@epitech.eu>
2023-10-31 15:29:30 +0100

    Add sign

--- components/about/Intro.vue
+++ components/about/Intro.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col gap-3 sm:ml-4">
+  <div class="relative flex flex-col gap-3 sm:ml-4">
     <h3 class="text-muted text-lg">Intro</h3>
     <div class="flex flex-col gap-4 text-main">
       <p>
@@ -17,6 +17,7 @@
         {{ $t("about.intro.part4") }}
       </p>
     </div>
+    <Signature class="absolute w-40 hidden sm:block -bottom-1/4 right-0" />
   </div>
 </template>
 
