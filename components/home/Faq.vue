7011781311998bf4b2cc4c96c3b069a5ca6989ea
HugoRCD <hugo.richard@epitech.eu>
2023-11-01 17:06:08 +0100

    Init faq section

---
+++ components/home/Faq.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { servicesAndToolstack, pricing } from "~/data/faq";
+const { locale } = useI18n();
+
+const items = computed(() => {
+  return [
+    {
+      label: servicesAndToolstack.title[locale.value as "en" | "fr"],
+      slot: "services-and-toolstack",
+    },
+    {
+      label: pricing.title[locale.value as "en" | "fr"],
+      slot: "pricing",
+    },
+  ];
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center space-y-8">
+    <div class="flex flex-col items-center justify-center gap-2">
+      <h3 class="text-4xl font-bold font-testimonial text-white-shadow">
+        {{ $t("faq.title") }}
+      </h3>
+      <p class="text-sm text-center text-muted">
+        {{ $t("faq.description") }}
+      </p>
+    </div>
+    <div>
+      <UTabs :items="items" orientation="horizontal" :ui="{ list: { marker: { rounded: 'rounded-full' }, rounded: 'rounded-full' } }">
+        <template #services-and-toolstack="{ item }">
+          <div class="flex items-center gap-2 text-shadow-sm" style="font-size: 1rem">
+            {{ $t(`faq.services-and-toolstack`) }}
+          </div>
+        </template>
+        <template #pricing="{ item }">
+          <div class="flex items-center gap-2 text-shadow-sm" style="font-size: 1rem">
+            {{ $t(`faq.pricing`) }}
+          </div>
+        </template>
+      </UTabs>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- data/faq.ts
+++ data/faq.ts
@@ -1,1 +1,54 @@
-export type Faq = {};
+export type Faq = {
+  title: {
+    en: string;
+    fr: string;
+  };
+  faq: {
+    title: {
+      en: string;
+      fr: string;
+    };
+    content: {
+      en: string;
+      fr: string;
+    };
+  }[];
+};
+
+export const servicesAndToolstack: Faq = {
+  title: {
+    en: "Services and toolstack",
+    fr: "Services et outils",
+  },
+  faq: [
+    {
+      title: {
+        en: "What services do you offer?",
+        fr: "Quels services offrez-vous?",
+      },
+      content: {
+        en: "We offer a wide range of services, from web development to mobile development, including design and consulting. You can find more information on our services page.",
+        fr: "Nous offrons une large gamme de services, du développement web au développement mobile, en passant par le design et le consulting. Vous pouvez trouver plus d'informations sur notre page services.",
+      },
+    },
+  ],
+};
+
+export const pricing: Faq = {
+  title: {
+    en: "Pricing",
+    fr: "Tarification",
+  },
+  faq: [
+    {
+      title: {
+        en: "How much does it cost?",
+        fr: "Combien ça coûte?",
+      },
+      content: {
+        en: "We offer a wide range of services, from web development to mobile development, including design and consulting. You can find more information on our services page.",
+        fr: "Nous offrons une large gamme de services, du développement web au développement mobile, en passant par le design et le consulting. Vous pouvez trouver plus d'informations sur notre page services.",
+      },
+    },
+  ],
+};
