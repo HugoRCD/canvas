14ea0d849c2ff9dad7104d924e4505e82b8b4e9e
HugoRCD <hugo.richard@epitech.eu>
2023-11-03 15:23:16 +0100

    Small text update

--- components/layout/InfoWrapper.vue
+++ components/layout/InfoWrapper.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
   <section class="flex flex-col max-w-4xl mx-auto p-7 mt-4 sm:mt-20">
     <h1 class="text-4xl text-center font-bold font-testimonial text-white-shadow">{{ $t(`${page}.title`) }}</h1>
-    <h2 class="text-lg text-center text-muted">{{ $t(`${page}.description`) }}</h2>
+    <h2 class="text-lg text-center font-light italic text-muted">{{ $t(`${page}.description`) }}</h2>
     <div class="flex justify-center my-3" v-if="page === 'contact'">
       <SettingsAvailability background />
     </div>
