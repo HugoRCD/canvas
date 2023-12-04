033401f3122e1c4f72b20e99fb21b456adfe4342
HugoRCD <hugo.richard@epitech.eu>
2023-12-04 17:07:34 +0100

    Remove multi domain

--- components/settings/LanguageToggle.vue
+++ components/settings/LanguageToggle.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-const switchLocalePath = useSwitchLocalePath();
 const { locale: current, setLocaleCookie } = useI18n();
 
 const locales = [
-  { code: "en", name: "English", domain: "https://hrcd.me" },
-  { code: "fr", name: "Français", domain: "https://hrcd.fr" },
+  { code: "en", name: "English" },
+  { code: "fr", name: "Français" },
 ];
 
 const currentLocale = computed(() => {
@@ -19,18 +18,11 @@ watch(current, (newLocale) => {
 <template>
   <div class="z-99 bg-zinc-900/90 backdrop-blur-xl flex items-center rounded-lg px-3 py-1 gap-3 border border-white/10">
     <ClientOnly>
-      <DevOnly>
-        <div class="cursor-pointer select-none bg-main rounded-lg px-3 py-1" v-for="locale in locales" :key="locale" @click="$i18n.locale = locale.code">
-          <span class="font-semibold" :class="locale.code === currentLocale.code ? 'text-white' : 'text-gray-500'">
-            {{ locale.code }}
-          </span>
-        </div>
-      </DevOnly>
-      <NuxtLink class="cursor-pointer select-none" v-for="locale in locales" :key="locale" :to="switchLocalePath(locale.code)">
+      <div class="cursor-pointer select-none" v-for="locale in locales" :key="locale" @click="$i18n.locale = locale.code">
         <span class="font-semibold" :class="locale.code === currentLocale.code ? 'text-white' : 'text-gray-500'">
           {{ locale.code }}
         </span>
-      </NuxtLink>
+      </div>
       <template #fallback>
         <div class="w-8 h-8" />
       </template>
