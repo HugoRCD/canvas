2f749653988e6a9793a9edbd8e19fc2e6fc02bb8
HugoRCD <hugo.richard@epitech.eu>
2023-12-05 13:49:50 +0100

    Make some css update / improve

--- components/project/Card.vue
+++ components/project/Card.vue
@@ -16,14 +16,14 @@ const img = useImage();
     :aria-label="project.name + ' project link'"
     :to="project.link"
     target="_blank"
-    class="group cursor-pointer flex flex-col gap-1 bg-zinc-900/80 rounded-lg p-1 shadow-2xl shadow-zinc-950/50 ring-1 ring-white/20 backdrop-blur-sm"
+    class="relative group cursor-pointer flex flex-col gap-1 bg-zinc-900/80 rounded-lg p-1 shadow-2xl shadow-zinc-950/50 border border-white/10 backdrop-blur-sm"
   >
     <div class="flex gap-1 px-1 py-[2px]">
       <div class="rounded-full w-2 h-2 bg-red-500/90 sm:bg-white/10 group-hover:bg-red-500/90 transition-all duration-300" />
       <div class="rounded-full w-2 h-2 bg-yellow-500/90 sm:bg-white/10 group-hover:bg-yellow-500/90 transition-all duration-300" />
       <div class="rounded-full w-2 h-2 bg-green-500/90 sm:bg-white/10 group-hover:bg-green-500/90 transition-all duration-300" />
     </div>
-    <div class="relative flex justify-center h-56 overflow-hidden rounded-lg">
+    <div class="flex justify-center h-56 overflow-hidden rounded-lg">
       <NuxtImg
         :placeholder="img(`${project.image}`)"
         width="1536"
@@ -32,8 +32,10 @@ const img = useImage();
         :src="project.image"
         :aria-label="project.name + ' project image'"
       />
-      <div class="absolute bottom-0 sm:w-2/3 border border-white/10 shadow-md backdrop-blur-md rounded-t-lg px-2 py-1">
-        <div class="flex gap-2 items-center justify-between px-1 py-1">
+    </div>
+    <div class="absolute w-full bottom-0 flex justify-center">
+      <div class="sm:w-2/3 border-t border-r border-l border-white/10 border-b-transparent shadow-md backdrop-blur-md rounded-t-lg px-4 py-[5px]">
+        <div class="flex gap-2 items-center justify-between">
           <div class="flex items-center gap-2">
             <component
               :is="project.logo"
@@ -47,7 +49,7 @@ const img = useImage();
               <span class="text-sm font-semibold text-white/90 whitespace-nowrap">
                 {{ project.name }}
               </span>
-              <span class="text-muted whitespace-nowrap text-xs">
+              <span class="text-neutral-500 whitespace-nowrap text-xs">
                 {{ project.release === "soon" ? $t("global.soon") + "..." : project.release }}
               </span>
             </div>
